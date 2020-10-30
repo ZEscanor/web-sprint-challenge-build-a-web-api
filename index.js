bf9f232ccd44ebebded4c8ef256bb153197e0077
Yinusa <yamusa2@uic.edu>
2020-10-30 10:39:35 -0500

    ADDED CRUD FUNCTIONALITY

--- index.js
+++ index.js
@@ -1,14 +1,168 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require("express")
 
-Sing along:
+const action = require("./data/helpers/actionModel")
+const project = require("./data/helpers/projectModel")
+const PORT = 5000;
+const server = express();
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+server.use(express.json())
+//server.use(action);
+//server.use(project)
+//server.use(logger)
 
-Go code!
-*/
+
+server.post("/actions",[validatePost],(req,res)=>{
+action.insert(req.body)
+.then(data=>{
+    res.status(200).json({successmessage:`success you got ${data}`})
+})
+.catch(error=>{
+    res.status(400).json({errormessage:"id not found"})
+})
+})
+
+server.put("/actions/:id",[validatePost],(req,res)=>{
+    console.log(req.params,req.body)
+    action.update(req.params.id,req.body)
+   
+    .then(
+        res.status(200).json({message:"success"})
+      )
+      .catch(error=>{
+        res.status(400).json({errorMessage:"not found"})
+      })
+})
+
+server.delete("/actions/:id", (req,res)=>{
+    action.remove(req.params.id)
+    .then(data=>{
+        res.status(201).json(data)
+    })
+    .catch(error=>{
+     res.status(500).json({errormessage:"Id doesnt exist"})
+    })
+    
+})
+
+server.get("/actions/:id",(req,res)=>{
+    console.log(req.params)
+    action.get(req.params.id)
+    .then(data=>{
+        res.status(200).json(data)
+    })
+    .catch(error=>{
+        res.status(400).json({errormessage:"id not found"})
+    })
+})
+
+
+
+
+server.post("/projects",[validateProject],(req,res)=>{
+    project.insert(req.body)
+    .then(data=>{
+        res.status(200).json({successmessage:`success you got ${data}`})
+    })
+    .catch(error=>{
+        res.status(400).json({errormessage:"id not found"})
+    })
+    })
+
+    server.put("/projects/:id",[validateProject],(req,res)=>{
+        console.log(req.params,req.body)
+        project.update(req.params.id,req.body)
+       
+        .then(
+            res.status(200).json({message:"success"})
+          )
+          .catch(error=>{
+            res.status(400).json({errorMessage:"not found"})
+          })
+    })
+
+    server.delete("/projects/:id", (req,res)=>{
+        project.remove(req.params.id)
+        .then(data=>{
+            res.status(201).json(data)
+        })
+        .catch(error=>{
+         res.status(500).json({errormessage:"Id doesnt exist"})
+        })
+        
+    })
+
+
+server.get("/projects/:id",(req,res)=>{
+    console.log(req.params)
+    project.get(req.params.id)
+    .then(data=>{
+        res.status(200).json(data)
+    })
+    .catch(error=>{
+        res.status(400).json({errormessage:"id not found"})
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.get('*', (req,res)=>{
+    res.send("Let's Begin Our Sprint!")
+});
+
+server.listen(PORT,()=>{
+    console.log(`Server Running on PORT ${PORT}`)
+})
+
+
+
+
+
+
+function validateProject(req, res, next) {
+    if(!req.body){
+      res.status(401).json({message:"You have no body"})
+  }
+  else if(!req.body.name){
+    res.status(401).json({message:"Missing some key elements name"})
+  }
+  else if(!req.body.description){
+    res.status(401).json({message:"Missing some key elements description"})
+
+  }
+  else{
+    next();
+  }
+  }
+
+function validatePost(req, res, next) {
+    // do your magic!
+    if(!req.body){
+      res.status(401).json({message:"Missing some key elements NAME"})
+  }
+  else if(!req.body.project_id){
+    res.status(401).json({message:"Missing some key elements proj id"})
+  }
+  else if(!req.body.description || !req.body.notes){
+    res.status(401).json({message:"Missing some key elements desc or notes"})
+
+  }
+  else{
+    next();
+  }
+  }
+  
+  module.exports = server;
